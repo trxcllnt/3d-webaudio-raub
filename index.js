adfaa1781bdf6c6aff2b5e193f4e85b342cc2e71
raub <raubtierxxx@gmail.com>
2018-02-26 17:55:19 +0300

    examples wip

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const webaudio = require('web-audio-api');
+const Speaker  = require('speaker');
+
 
 module.exports = core => {
 	
@@ -7,8 +10,19 @@ module.exports = core => {
 		return;
 	}
 	
-	core.webaudio = {};
+	core.webaudio = webaudio;
 	
-	require('./js')(core);
+	window.AudioContext = webaudio.AudioContext;
+	
+	
+	const context = new webaudio.AudioContext();
+	
+	context.outStream = new Speaker({
+		channels   : context.format.numberOfChannels,
+		bitDepth   : context.format.bitDepth,
+		sampleRate : context.sampleRate,
+	})
+	
+	core.three.AudioContext.setContext(context);
 	
 };
